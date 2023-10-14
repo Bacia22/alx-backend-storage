726eba97f8c927bf2889eaf18dc3c55e626266a0
Bacia22 <adolfobacia@yahoo.com>
2023-10-14 20:40:58 +0000

    3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,3 @@
--- Lists all bands with Glam rock as their main atyle, ranked by their longevity
+-- Lists all bands with Glam rock as their main style, ranked by their longevity
 SELECT band_name, (IFNULL(split, 2022) - formed)
-AS lifespan FROM metal_bands WHEE style LIKE '%Glam rock% ORDER BY lifespan DESC;
+AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock% ORDER BY lifespan DESC;
