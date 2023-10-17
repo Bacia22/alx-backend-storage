ed78ad9fa9051ceeceb630d8f9782e977312471d
Bacia22 <adolfobacia@yahoo.com>
2023-10-17 21:21:04 +0000

    9-insert_school.py

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -2,6 +2,6 @@
 """ MongoDB Operations with Python using pymongo """
 
 
-def insert_school(mongo_collection, **Kwargs):
-    """ Insert a new document in a collection based on kwargs """
+def insert_school(mongo_collection, **kwargs):
+    """ Inserts a new document in a collection based on kwargs """
     return mongo_collection.insert(kwargs)
